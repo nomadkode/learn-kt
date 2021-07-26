969466151142af48bf4107b2bcbdffae0a100d1a
ramset <setiawanramadyan@gmail.com>
2021-07-26 14:00:24 +0700

    printing to console

---
+++ learn-cpp/02_basic/printing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "hallo sodaraku\n";
+  std::cout << "di jakarta";
+  std::cout << "apa kabarnya?";
+  std::cout << "baik baik saja" << std::endl;
+  std::cin.get();
+  return 0;
+}
