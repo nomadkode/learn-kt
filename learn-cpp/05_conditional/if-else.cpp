80dde23a5bdac723adbbdd99914d125d9b87b014
ramset <setiawanramadyan@gmail.com>
2021-07-28 21:24:58 +0700

    conditional if

---
+++ learn-cpp/05_conditional/if-else.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+
+  cout << "masukan angka = ";
+  cin >> a;
+
+  // if statement
+  if (a < 3)
+  {
+    cout << "haloo" << endl;
+  }
+
+  cout << "selesai" << endl;
+
+  cin.get();
+  return 0;
+}
