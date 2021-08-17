4900917f59a11330d34d6db2d61bb2f971a52a7b
ramset <setiawanramadyan@gmail.com>
2021-08-17 14:20:59 +0700

    4 (empat) for loop

--- learn-cpp/07_loops/perulangan.cpp
+++ learn-cpp/07_loops/perulangan.cpp
@@ -17,7 +17,28 @@ int main() {
     cout << "horee ";
     cout << b << endl;
     b++;
-  } while (b <= 5);
+  } while(b <= 5);
+
+  // for loop 1
+  for(int count = 1; count <= 10; count++){
+    cout << count << endl;
+  }
+
+  // for loop 2
+  for(int i = 1; i <= 10; i += 2){
+    cout << i << endl;
+  }
+
+  // for loop 3
+  for(int j = 1; j >= -10; j--){
+    cout << j << endl;
+  }
+
+  // for loop 4
+  int total = 0;
+  for(int c = 1; c <= 10; total += c, c++){
+    cout << c << " || " << total << endl;
+  }
 
   cout << "selesai" << endl;
   cin.get();
