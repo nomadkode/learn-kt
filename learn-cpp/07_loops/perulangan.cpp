fec1d7c42cfb440a56a2c2651db7caf4e361324c
ramset <setiawanramadyan@gmail.com>
2021-08-17 13:56:41 +0700

    do while

--- learn-cpp/07_loops/perulangan.cpp
+++ learn-cpp/07_loops/perulangan.cpp
@@ -6,10 +6,18 @@ int main() {
   // while
   int a = 5;
   while(a <= 10) {
-    cout << "horee ";
+    cout << "hore ";
     cout << a << endl;
     a += 1;
   }
+
+  // do while
+  int b = 1;
+  do {
+    cout << "horee ";
+    cout << b << endl;
+    b++;
+  } while (b <= 5);
 
   cout << "selesai" << endl;
   cin.get();
