c085deb0ebcc2cfb8f822b2fed675228a75498e0
ramset <setiawanramadyan@gmail.com>
2021-08-17 13:45:49 +0700

    while

---
+++ learn-cpp/07_loops/perulangan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // while
+  int a = 5;
+  while(a <= 10) {
+    cout << "horee ";
+    cout << a << endl;
+    a += 1;
+  }
+
+  cout << "selesai" << endl;
+  cin.get();
+  return 0;
+}
